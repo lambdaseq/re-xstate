a47400b9ea872dc270680d2dc8c6809a30c96288
Nikolas Pafitis <pafitisnick@gmail.com>
2023-07-21 01:15:09 +0300

    Fixed build/deploy configuration

--- build.clj
+++ build.clj
@@ -3,10 +3,9 @@
   (:require [clojure.tools.build.api :as b]
             [deps-deploy.deps-deploy :as dd]))
 
-(def lib 'net.clojars.drbuchkov/re-xstate)
-(def version "0.1.0")
-#_; alternatively, use MAJOR.MINOR.COMMITS:
-        (def version (format "1.0.%s" (b/git-count-revs nil)))
+(def lib 'io.github.drbuchkov/re-xstate)
+(def version (b/git-process {:git-args ["describe" "--tags"]}))
+
 (def class-dir "target/classes")
 
 (defn test "Run all the tests." [opts]
